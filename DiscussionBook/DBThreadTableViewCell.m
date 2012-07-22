a983a8589c560d297870a4e2674cb2cecfbece94
Jacob Relkin <jacobrelkin@gmail.com>
2012-07-22 11:19:44 -0700

    Date and time formatting and positioning of date label in DBThreadTableViewCell
    Signed-off-by: Jacob Relkin <jacobrelkin@gmail.com>

--- DiscussionBook/DBThreadTableViewCell.m
+++ DiscussionBook/DBThreadTableViewCell.m
@@ -11,6 +11,41 @@
 #import "FBUser.h"
 #import "DBPaddedLabel.h"
 
+UIKIT_STATIC_INLINE NSDateFormatter *DBRelativeDateFormatter() {
+    static NSDateFormatter *formatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        formatter = [[NSDateFormatter alloc] init];
+        [formatter setDateStyle:NSDateFormatterShortStyle];
+        [formatter setDoesRelativeDateFormatting:YES];
+    });
+    
+    return formatter;
+}
+
+UIKIT_STATIC_INLINE NSDateFormatter *DBTimeFormatter() {
+    static NSDateFormatter *formatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        formatter = [[NSDateFormatter alloc] init];
+        [formatter setTimeStyle:NSDateFormatterShortStyle];
+    });
+    
+    return formatter;
+}
+
+UIKIT_STATIC_INLINE BOOL dateIsToday(NSDate *date) {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+
+    NSUInteger flags = (NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit);
+    NSDateComponents *components = [calendar components:flags
+                                               fromDate:date];
+    NSDateComponents *nowComponents = [calendar components:flags
+                                                  fromDate:[NSDate date]];
+    
+    return [components isEqual:nowComponents];
+}
+
 @interface DBThreadTableViewCell()
 
 @property (nonatomic, weak) IBOutlet UILabel *titleLabel;
@@ -38,15 +73,27 @@
 }
 
 - (void)setRepresentedObject:(FBGroupThread *)representedObject {
-    [_commentCountLabel setPadding:UIEdgeInsetsMake(1, 3, 1, 3)];
-    [_commentCountLabel setBackgroundColor:[UIColor grayColor]];
-    [_commentCountLabel setTextColor:[UIColor whiteColor]];
-    [_commentCountLabel setHighlightedTextColor:[UIColor grayColor]];
-    [[_commentCountLabel layer] setCornerRadius:6];
-    [_commentCountLabel sizeToFit];
+    NSUInteger count = representedObject.comments.count;
+    [_commentCountLabel setHidden:count == 0];
+    
+    if(count) {
+        [_commentCountLabel setText:[NSString stringWithFormat:@"%d", count]];
+        [_commentCountLabel setPadding:UIEdgeInsetsMake(1, 3, 1, 3)];
+        [_commentCountLabel setBackgroundColor:[UIColor grayColor]];
+        [_commentCountLabel setTextColor:[UIColor whiteColor]];
+        [_commentCountLabel setHighlightedTextColor:[UIColor grayColor]];
+        [[_commentCountLabel layer] setCornerRadius:4];
+        [_commentCountLabel sizeToFit];
+    }
     
     _titleLabel.text   = [[representedObject fromUser] name];
-    _dateLabel.text    = @"Blah"; //TODO: utilize date formatter here
+    
+    NSDate *updatedDate  = [representedObject updatedDate];
+    NSDateFormatter *formatter = (dateIsToday(updatedDate)) ? DBTimeFormatter() : DBRelativeDateFormatter();
+    
+    _dateLabel.text    = [formatter stringFromDate:updatedDate];
+    [_dateLabel sizeToFit];
+    
     _detailsLabel.text = [representedObject message];
 }
 
@@ -73,10 +120,13 @@
     CGRect dateFrame = [_dateLabel frame];
     CGRect messageFrame = [_detailsLabel frame];
     
-    countFrame.origin.x = CGRectGetMaxX(dateFrame) - countFrame.size.width;
+    countFrame.origin.x = self.bounds.size.width - countFrame.size.width - 10;
     countFrame.origin.y = roundf(CGRectGetMidY(messageFrame) - (countFrame.size.height/2));
     
     [_commentCountLabel setFrame:countFrame];
+    
+    dateFrame.origin.x = self.bounds.size.width - dateFrame.size.width - 10;
+    [_dateLabel setFrame:dateFrame];
 }
 
 @end
